154e46b2e45843856c8a820150fed4b6a8c6c9d6
Cipi01 <cipriangheorghe_capata@yahoo.com>
2023-10-25 16:06:15 +0300

    Added contact page and button to start, with prevention from starting script if user or pass fields empty TODO: Complete mixed choice; create input field in contact page for emails

---
+++ pages/contact.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("CONTACT")
+st.divider()
+
+st.subheader("This web application was made by Capătă Ciprian."
+             " Please reach out for any questions/ information by:")
+
+st.markdown("#")
+
+st.text("Phone: +40075 870 5895")
+st.text("Gmail: cipriangheorghecapata@gmail.com")
--- pages/main_page.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.stop()
-st.subheader("EON/ Gas utility")
-st.divider()
-
-
-st.subheader("Electrica/ Electric utility")
-st.divider()
