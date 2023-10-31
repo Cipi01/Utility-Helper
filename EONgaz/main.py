fa6a642a2f00bc33e52f9691d1a4497fd828c6cc
Cipi01 <cipriangheorghe_capata@yahoo.com>
2023-10-31 20:53:08 +0200

    added streamlit args to main files v.4

--- EONgaz/main.py
+++ EONgaz/main.py
@@ -3,7 +3,7 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 
 
 def get_data_gaz(username, password):
@@ -17,7 +17,7 @@ def get_data_gaz(username, password):
     options.add_argument("--window-size=1920x1080")
     options.add_argument("--disable-features=VizDisplayCompositor")
 
-    driver = webdriver.Chrome(options)
+    driver = webdriver.Firefox(options)
     driver.implicitly_wait(10)
     driver.get('https://www.eon.ro/myline/login')
     driver.implicitly_wait(10)
